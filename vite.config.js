01789beb23b6a16867b3c7d782510ac453e546fb
saifulislam735 <mohimshihab735@gmail.com>
2024-04-15 21:43:46 +0600

    git first deployment process done

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base:'/Developer-Vlog-Site-React/',
+  plugins: [react()],
+})
